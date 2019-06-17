dbd988d2006f2c50595cf18f0ee1e0fbef8093a1
earl-stephens <stephensearl@hotmail.com>
2019-06-17 14:07:25 -0600

    initial commit

---
+++ spec/node_spec.rb
@@ -0,0 +1,11 @@
+require './lib/node'
+
+RSpec.describe 'node class' do
+  node1 = Node.new(title: 'Twister', score: 85)
+
+  expect(node1).to be_an_instance_of(Node)
+  expect(node1.title).to eq('Twister')
+  expect(node1.score).to eq(85)
+  expect(node1.left_link).to eq(nil)
+  expect(node1.right_link).to eq(nil)
+end
